48b981b3f6c799930642d9bdbba129eab35e0dd8
unknown <omobowalerohma1998@gmail.com>
2023-08-16 11:17:44 +0100

    validated form input

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/signup.css'
 import { useForm} from "react-hook-form" 
 import Button from '../components/Button'
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+
 
 function SignUp() {
-  const {register}=useForm()
+  const {register,handleSubmit,formState:{errors,isSubmitting,isSubmitSuccessful}}=useForm()
+  const [showPassword,setShowPassword]=useState(false)
+  function togglePasswordType(){
+    setShowPassword(!showPassword)
+  }
+  function submit(data){
+    console.log(isSubmitting)
+    // console.log(isSubmitSuccessful)
+  }
+
   return (
-    <section className='signup-container'>
+    <section className='signup-container signInUp-container'>
       <div className='signup-steps'>
         <div className='signup-step1'></div>        
       </div>
       <h2>Let's get started!</h2>
-      <form className='signup-form'>
+      <form className='signup-form' onSubmit={handleSubmit(submit)}>
        <label htmlFor='firstname'> First Name
-       <input {...register("firstname")} className='signup-input'type='text' id='firstname'/>
+       <input {...register("firstname",{required:"firstname is required",minLength:{value:2,message:"min lenght is 2"}})} className='signup-input'type='text' id='firstname'/>
+       <p className='error-message'>{errors.firstname?.message}</p>
        </label>
        <label htmlFor='lastname'> Last Name
-       <input {...register("lastname")}type='text' id='lastname'/>
+       <input {...register("lastname",{required:"lastname is required",minLength:{value:2,message:"min lenght is 2"}})}type='text' id='lastname'/>
+       <p className='error-message'>{errors.lastname?.message}</p>
        </label>
        <label htmlFor='email'>Email Address
-       <input {...register("email")}type='text' id='email'/>
+       <input {...register("email",{required:"email is required",pattern:
+       {value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message:'Invalid email format'}
+      })}type='text' id='email'/>
+       <p className='error-message'>{errors.email?.message}</p>
        </label>
-       <label htmlFor='password'>Password
-       <input {...register("password")}type='password' id='password'/>
+       <label htmlFor='password' className='signup-label'>Password
+       <div className='input-container'>
+       <input {...register("password",{required:"password is required",minLength:{value:5,message:'min length 5 characters'}})}type={`${showPassword?"text":"password"}`} id='password' className='signUp-password'/>
+       <p className='icon-right' onClick={togglePasswordType}>{showPassword?<FaEye/>:<FaEyeSlash/>}</p>
+       </div>
+       <p className='error-message'>{errors.password?.message}</p>
        </label> 
        <Button type='submit' text='Continue' classname='signup-btn'/>
       </form>
