be6646f9ee2cc2254c3d2e54e9610cda9fc4cb29
unknown <omobowalerohma1998@gmail.com>
2023-08-25 23:16:52 +0100

    nothing

--- src/components/InviteMatch.jsx
+++ src/components/InviteMatch.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/inviteMatch.css'
+import DatePicker from 'react-datepicker';
+
 
 import Button from './Button'
 import { useDispatch, useSelector } from 'react-redux'
@@ -19,8 +21,49 @@ function InviteMatch() {
     <div className='invite-match-container'>
       <h3>Invite</h3>
       <form className='invitation-form' onSubmit={openModal}>
-        <div>
-        INVITATION FORM CONTENT
+        <p>Please choose a scheduled date within the next two weeks </p>
+        <input type='date'/>
+        
+        
+        <p>Starting at ....</p>
+        <select  className="time-dropdown">
+      <option value="00:00">00:00</option> 
+      <option value="01:00">01:00</option> 
+      <option value="02:00">02:00</option> 
+      <option value="03:00">03:00</option> 
+      <option value="04:00">04:00</option> 
+      <option value="05:00">05:00</option> 
+      <option value="06:00">06:00</option> 
+      <option value="07:00">07:00</option> 
+      <option value="08:00">08:00</option> 
+      <option value="09:00">09:00</option> 
+      <option value="10:00">10:00</option> 
+      <option value="11:00">11:00</option> 
+      <option value="12:00">12:00</option> 
+      <option value="13:00">13:00</option> 
+      <option value="14:00">14:00</option>
+      <option value="15:00">15:00</option>  
+      <option value="16:00">16:00</option> 
+      <option value="17:00">17:00</option> 
+      <option value="18:00">18:00</option> 
+      <option value="19:00">19:00</option> 
+      <option value="20:00">20:00</option> 
+      <option value="21:00">21:00</option> 
+      <option value="22:00">22:00</option> 
+      <option value="23:00">23:00</option> 
+
+  </select>  
+ 
+        <p>as my ...</p>
+        <div className='step-2_options seeking-type'>   
+        <input  type="radio" id="partner" name='seeking_type' value="partner"/>
+          <label htmlFor="partner" >
+           <p>Partner</p>
+         </label>
+         <input  type="radio" id="opponent" name='seeking_type' value="opponent"/>
+         <label htmlFor="opponent">
+           <p>Opponent</p>
+         </label>
         </div>
       <Button  text='Send' type='submit' classname='invite_btn'/>
       </form>
