c2b3655c66c17eb23f2b9341f3cb6232088ee160
Divxsh <guptadivash70@gmail.com>
2023-08-31 22:06:14 +0530

    updated the logout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import {
   NavLink,
   Outlet,
   useNavigation,
-  useSearchParams,
+  useNavigate,
 } from "react-router-dom";
 import "../styles/layout.css";
 
@@ -14,12 +14,13 @@ import ProtectedRoute from "../components/ProtectedRoute";
 import InviteSentModal from "../components/InviteSentModal";
 import InviteAcceptedModal from "../components/InviteAcceptedModal";
 import { useDispatch, useSelector } from "react-redux";
-import { inviteMatch } from "../redux/slices/userSlice";
+import { authenticate, inviteMatch } from "../redux/slices/userSlice";
 import Loading from "../components/Loading";
 
 function AppLayout() {
   const [menu, setMenu] = useState(false);
   const navigation = useNavigation();
+  const navigate = useNavigate();
   const navigationState = navigation.state;
   const dispatch = useDispatch();
   function toggleMenu() {
@@ -36,7 +37,13 @@ function AppLayout() {
     borderRadius: "5px",
     boxShadow: "0 1.5px 3px 0 #000000",
   };
-  const isAuth =sessionStorage.getItem('isAuth');
+  const isAuth = sessionStorage.getItem('isAuth');
+  
+  function logoutHandler() {
+    dispatch(authenticate(false));
+    navigate('/');
+  }
+
   return (
     <div className="app-layout">
       {!isAuth && (
@@ -46,23 +53,28 @@ function AppLayout() {
       )}
       {isAuth && (
         <nav className={`${menu ? "" : "bottom-border"}`}>
+          {/* Logo */}
           <p className="logo nav-logo">
-            <NavLink to="/">Pickle Pals</NavLink>
+            <NavLink to="/profile">Pickle Pals</NavLink>
           </p>
+
+          {/* Nav Items */}
           <ul className="desktop-ul">
-            <NavLink to="/">Home</NavLink>
+            {/* <NavLink to="/">Home</NavLink> */}
             <ProtectedRoute to="/profile">Profile</ProtectedRoute>
-            <p onClick={closeInvite}>
+            <p onClick={closeInvite} style={{margin:"0px"}}>
               <ProtectedRoute to="/match/matchFound">Match</ProtectedRoute>
             </p>
             <ProtectedRoute to="/courts">Courts</ProtectedRoute>
+            <a href="#" onClick={logoutHandler}>Logout</a>
           </ul>
+
           {/* MOBILE HARMUBURGER MENU */}
           <div className="mobile-hamburger-menu">
             <BsList style={iconStyles} onClick={toggleMenu} />
             {menu && (
               <ul className={`mobile-ul ${menu ? "bottom-border" : ""}`}>
-                <NavLink to="/">Home</NavLink>
+                {/* <NavLink to="/">Home</NavLink> */}
                 <ProtectedRoute to="/profile">Profile</ProtectedRoute>
                 <p onClick={closeInvite}>
                   <ProtectedRoute to="/match/matchFound">Match</ProtectedRoute>
@@ -73,6 +85,7 @@ function AppLayout() {
           </div>
         </nav>
       )}
+
       {navigationState === "loading" && <Loading />}
       <InviteSentModal />
       <InviteAcceptedModal />
